1e11471f0e58415233349747101be39d64c1f4ef
Eric G. Wolfe <wolfe21@marshall.edu>
2010-10-28 21:31:28 -0400

    Add snmp cookbook. Signed-off-by: Eric G. Wolfe <wolfe21@marshall.edu>

---
+++ recipes/default.rb
@@ -0,0 +1,50 @@
+#
+# Cookbook Name:: snmp
+# Recipe:: default
+#
+# Copyright 2010, Eric G. Wolfe
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package "snmpd" do
+  case node[:platform]
+  when "centos","redhat"
+    package_name "net-snmp"
+  when "debian","ubuntu"
+    package_name "snmpd"
+  end
+  action :install
+end
+
+if node[:snmp][:install_utils]
+  case node[:platform]
+  when "centos","redhat"
+    package_name "net-snmp-utils"
+  when "debian","ubuntu"
+    package_name "snmp"
+  end
+  action :install
+end
+
+service node[:snmp][:service] do
+  action :start
+end
+
+template "/etc/snmp/snmpd.conf" do
+  mode "0644"
+  owner "root"
+  group "root"
+  source "snmpd.conf.erb"
+  notifies :restart, resources(:service => node[:snmp][:service])
+end
